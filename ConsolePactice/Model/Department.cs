c26b2afd16ce9e276f56defaa43e5a7c765bedd3
Shingyu Kang <rkdtlseb@naver.com>
2024-05-18 15:41:20 +0900

    초기 커밋.

---
+++ ConsolePactice/Model/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsolePactice.Model
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string ShortName { get; set; }
+        public string LongName { get; set; }
+    }
+}
